019a8e7a837a3968b92b7385047ae80a94ad0b7a
sainivas-yellapragada <sainivasyellapragada@gmail.com>
2025-01-30 10:56:13 +0530

    temp repo

---
+++ env/Lib/site-packages/djongo/__init__.py
@@ -0,0 +1,11 @@
+# This version of Djongo5 was made possible by
+# the generous contributions from:
+#
+#       * Zachary Sizemore
+#       * Wayne Van Son
+#       * Norman Niemer
+#       * Renz Ladia
+#       * thestick613
+#       * William Ferreira
+
+__version__ = "1.3.9"
